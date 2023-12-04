41bf7e1c3d3999fa0f5896223ff4a2e9c566c5e8
Vikram Naik <26vikramnaik@gmail.com>
2023-12-05 00:16:53 +0530

    Navbar and components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  SpecialOffer,
+  SuperQuality,
+  Services,
+  Subscribe,
+} from './sections'
+import Nav from './components/Nav'
+
 const App = () => (
   <main className="relative">
-    Nav
-    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-    <section className="padding">PopularProducts</section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding">Services</section>
-    <section className="padding">SpecialOffer</section>
-    <section className="padding bg-pale-blue">CustomerReviewa</section>
-    <section className="padding-x w-full py-16 sm:py-32">Subscribe</section>
-    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="padding bg-pale-blue">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x w-full py-16 sm:py-32">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
   </main>
 )
 
---
+++ src/sections/index.jsx
@@ -0,0 +1,19 @@
+import CustomerReviews from './CustomerReviews'
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Footer from './Footer'
+import Services from './Services'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import SpecialOffer from './SpecialOffer'
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  Subscribe,
+  CustomerReviews,
+  SpecialOffer,
+  Footer,
+}
