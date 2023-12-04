ca89d36d6b58e5fe01fcf8cccffe437baa5f2d10
Vikram Naik <26vikramnaik@gmail.com>
2023-12-04 23:16:06 +0530

    initial web app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">PopularProducts</section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding">Services</section>
+    <section className="padding">SpecialOffer</section>
+    <section className="padding bg-pale-blue">CustomerReviewa</section>
+    <section className="padding-x w-full py-16 sm:py-32">Subscribe</section>
+    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+  </main>
+)
+
+export default App
