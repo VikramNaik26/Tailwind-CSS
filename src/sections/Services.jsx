a7c111f079807929b015b311e30892a0fd758a5c
Vikram Naik <26vikramnaik@gmail.com>
2023-12-06 23:08:13 +0530

    services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,14 @@
+import { support } from '../assets/icons'
+import ServiceCard from '../components/ServiceCard'
+import { services } from '../constants'
+
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
   )
 }
 export default Services
