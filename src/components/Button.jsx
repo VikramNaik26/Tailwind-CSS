0cb4ca266cd830c4f7240f8c7870499a7376e56b
Vikram Naik <26vikramnaik@gmail.com>
2023-12-06 21:23:43 +0530

    Super quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,11 +5,13 @@ const Button = ({ label, iconURL }) => {
       className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat txet-lg leading-none bg-coral-red rounded-full text-white border-coral-red"
     >
       {label}
-      <img
-        src={iconURL}
-        alt="Arrow Right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="Arrow Right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   )
 }
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,6 +1,43 @@
+import Button from '../components/Button'
+import { shoe8 } from '../assets/images'
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          we provide you
+          <span className="text-coral-red"> super&nbsp;</span>
+          <span className="text-coral-red"> quality&nbsp;</span>
+          shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ut
+          hic dolorem deserunt. Optio voluptate beatae quos earum, incidunt
+          fugit ut! Magni provident doloremque laborum neque accusamus aliquam
+          vel at.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+      <div className="flex flex-1 justify-center items-center">
+        <img
+          src={shoe8}
+          alt="Shoe Collection"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
   )
 }
 export default SuperQuality
